3fafb934e2884743c518b2fb0fb9bd82532a5e98
unknown <viola.azatyan@mail.ru>
2023-02-11 01:06:37 +0400

    First commit

---
+++ Password/Program.cs
@@ -0,0 +1,31 @@
+﻿using Password;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            User[] users = new User[]
+            {
+                new User { FirstName = "Argishti", LastName = "Bejanyan", Age = 32, Login = "argishti", Password = "password1" },
+                new User { FirstName = "Irina", LastName = "Safaryan", Age = 24, Login = "irina", Password = "password2" },
+                new User { FirstName = "Rafayel", LastName = "Dallaqyan", Age = 45, Login = "rafayel", Password = "password3" }
+            };
+
+            User user = new User();
+            Console.WriteLine(user.CheckUser(users, "argishti", "password1"));
+            Console.WriteLine(user.CheckUser(users, "argishti", "password3"));
+        }
+    }
+        
+    }
+
---
+++ Password/User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Login { get; set; }
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { _password = HashPassword(value); }
+        }
+
+        public string HashPassword(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public bool CheckPassword(string password)
+        {
+            return HashPassword(password) == _password;
+        }
+
+        public string CheckUser(User[] users, string login, string password)
+        {
+            foreach (User user in users)
+            {
+                if (user.Login == login && user.CheckPassword(password))
+                {
+                    return "User exists and password is correct."; ;
+                }
+            }
+            return "User does not exist or password is incorrect.";
+        }
+    }
+}
+
+
